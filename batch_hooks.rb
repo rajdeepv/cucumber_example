9e8753a2eb7472fd97155735e20677458320c4a0
Rajdeep Varma <mail.rajvarma@gmail.com>
2020-02-12 23:27:10 +0000

    refactor

--- batch_hooks.rb
+++ batch_hooks.rb
@@ -1,25 +1,42 @@
 require 'parallel_cucumber/dsl'
 require 'json'
 
-ParallelCucumber::DSL.after_batch do |results, batch_id, env, scenario_details|
-  redis = Redis.new(url: ENV['REDIS'])
-  not_passed_scenarios = results.reject { |_key, val| val == :passed }.keys
-  not_passed_scenarios.map(&:to_s).each do |scenario|
-    if redis.get('rerun').include?(scenario)
-      next
-    end
-    details = JSON.parse(redis.rpop(batch_id))
-    if details[scenario]["exception_class"] == "BusinessError"
-      next
-    end
-    puts "=#=#=#=#=#=#=#=# Requeue #{scenario}, Reason: #{details[scenario]['exception_class']} =#=#=#=#=#=#=#=#"
-    redis.lpush("kill-worker-#{ENV['WORKER_INDEX']}","Killed in After Batch")
-    puts "=#=#=#=#=#=#=#=# Kill #{ENV['WORKER_INDEX']}, } =#=#=#=#=#=#=#=#"
-    redis.lpush('rerun', scenario)
-    redis.rpush('skanky', scenario)
+ParallelCucumber::DSL.after_batch do |results, batch_id, env|
+  not_passed_scenarios = results.reject {|_key, val| val == :passed}.keys.map(&:to_s)
+  not_passed_scenarios.each do |scenario|
+
+    next if ran_twice?(scenario)
+
+    next if failed_with_business_reason?(scenario, batch_id)
+
+    mark_worker_as_sick(env['WORKER_INDEX'])
+    requeue(scenario)
   end
 end
 
 #ParallelCucumber::DSL.after_workers do
 #  `killall -9 node`
 #end
+
+def ran_twice?(scenario)
+  redis.get('rerun').include?(scenario)
+end
+
+def failed_with_business_reason?(scenario, batch_id)
+  details = JSON.parse(redis.rpop(batch_id))
+  details[scenario]["exception_class"] == "BusinessError"
+end
+
+def redis
+  @redis ||= Redis.new(url: ENV['REDIS'])
+end
+
+def requeue(scenario)
+  puts "=#=#=#=#=#=#=#=# Requeue #{scenario} =#=#=#=#=#=#=#=#"
+  redis.lpush('rerun', scenario)
+  redis.rpush('skanky', scenario)
+end
+
+def mark_worker_as_sick(worker_index)
+  redis.lpush("sick-worker-#{worker_index}", "Killed in After Batch")
+end
--- worker_precheck.rb
+++ worker_precheck.rb
@@ -1,3 +1,3 @@
 require 'redis'
 redis = Redis.new(url: ENV['REDIS'])
-exit(1) if redis.get("kill-worker-#{ENV['WORKER_INDEX']}").any?
+exit(1) if redis.get("sick-worker-#{ENV['WORKER_INDEX']}").any?
